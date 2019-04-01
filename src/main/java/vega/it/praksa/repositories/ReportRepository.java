90b69b5e62acef51c534869901b17092f80b246f
Jovan42 <jovan0042@gmail.com>
2019-04-01 16:56:25 +0200

    Bug fix

--- src/main/java/vega/it/praksa/repositories/ReportRepository.java
+++ src/main/java/vega/it/praksa/repositories/ReportRepository.java
@@ -1,13 +1,17 @@
 package vega.it.praksa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import vega.it.praksa.model.Category;
+import vega.it.praksa.model.Project;
 import vega.it.praksa.model.Report;
+import vega.it.praksa.model.TeamMember;
 
 import java.util.Date;
 import java.util.List;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
     //TODO: Test
-    List<Report> findAllByTeamMemberAndProjectAndCategoryAndDateLessThanEqualAndDateGreaterThanEqual(Date startDate,
-                                                                                                     Date endDate);
+    List<Report> findAllByTeamMemberAndProjectAndCategoryAndDateBetween (TeamMember teamMember, Project project,
+                                                                         Category category, Date startDate,
+                                                                         Date endDate);
 }
