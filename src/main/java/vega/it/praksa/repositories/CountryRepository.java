7ec3781cd09d55b5340cfa359d425f5ff2842fcc
Jovan42 <jovan0042@gmail.com>
2019-04-01 16:43:27 +0200

    Typos

--- src/main/java/vega/it/praksa/repositories/CountryRepository.java
+++ src/main/java/vega/it/praksa/repositories/CountryRepository.java
@@ -3,5 +3,5 @@ package vega.it.praksa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vega.it.praksa.model.Country;
 
-public interface CountryRepositry extends JpaRepository<Country, Long> {
+public interface CountryRepository extends JpaRepository<Country, Long> {
 }
--- src/main/java/vega/it/praksa/repositories/TeamMemberRepositry.java
+++ src/main/java/vega/it/praksa/repositories/TeamMemberRepositry.java
@@ -3,5 +3,5 @@ package vega.it.praksa.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import vega.it.praksa.model.TeamMember;
 
-public interface TeamMmberRepositry extends JpaRepository<TeamMember, Long> {
+public interface TeamMemberRepositry extends JpaRepository<TeamMember, Long> {
 }
