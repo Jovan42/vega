ade50835ceb1db77e30ae13b1afb8a94c2aa914b
Jovan <jovan0042@gmail.com>
2019-04-02 11:18:06 +0000

    Revert "Work Delete"
    This reverts commit 5ee02de927433ac691bbbadc35154a89bfbf2d96

--- src/main/java/vega/it/praksa/mappers/DtoMapper.java
+++ src/main/java/vega/it/praksa/mappers/DtoMapper.java
@@ -23,4 +23,7 @@ public interface DtoMapper {
 
     TeamMember teamMemberDtoToTeamMember(TeamMemberDto teamMemberDto);
     TeamMemberDto teamMemberToTeamMemberDto(TeamMember teamMember);
+
+    Work workDtoToWork (WorkDto workDto);
+    WorkDto workToWorkDto(Work work);
 }
