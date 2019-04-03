ce8bd6ddb541d12d460f9c7ea00c20a5e083ae74
Jovan42 <jovan0042@gmail.com>
2019-04-03 16:49:27 +0200

    Uniques

--- src/main/java/vega/it/praksa/model/TeamMember.java
+++ src/main/java/vega/it/praksa/model/TeamMember.java
@@ -12,7 +12,7 @@ public class TeamMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique=true)
     private String username;
     @Column(nullable = false)
     private String password;
