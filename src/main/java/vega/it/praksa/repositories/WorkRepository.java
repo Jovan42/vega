8fc72fb473f59f343211b731ab71b42279ec79cd
Jovan42 <jovan0042@gmail.com>
2019-04-03 11:19:37 +0200

    Work search

--- src/main/java/vega/it/praksa/repositories/WorkRepository.java
+++ src/main/java/vega/it/praksa/repositories/WorkRepository.java
@@ -9,14 +9,12 @@ import java.util.List;
 
 public interface WorkRepository extends JpaRepository<Work, Long> {
 
-    //@Query("select * from table where month(birthdate)=03 and year(birthdate)=1990")
-    //List<WorkDto> findAllByDate_DayAndDate_MonthAndDate_Year(int day, int month, int year);
-
     @Query("select e from Work e where year(e.date) = ?1 and  month(e.date) = ?2 and day(e.date) = ?3")
     List<Work> getForDay(int year, int month, int day );
 
-    //@Query("select e from Work where e.Category.Name = ?1")
-    //List<Work> search(String name);
-    //List<Work> findAllByProject_LeadOrProject_ClientOrProjectOrCategoryOrDateLessThanEqualAndDateGreaterThanEqual
-      //      (TeamMember teamMember, Client client, Project project, Category category, Date endDate, Date startDate);
+
+    @Query("select w from Work w where (w.category.id = ?1 or ?1 = null) and (w.project.id = ?2 or ?2 = null)" +
+    "and (w.project.client.id = ?3 or ?3 = null) and (w.project.lead.id = ?4 or ?4 = null) and " +
+            "(w.date >= ?5 or ?5 = null) and (w.date <= ?6 or ?6 = null)")
+    List<Work> search(Long categoryId, Long projectId, Long clientId, Long leadId, Date startDate, Date endDate);
 }
