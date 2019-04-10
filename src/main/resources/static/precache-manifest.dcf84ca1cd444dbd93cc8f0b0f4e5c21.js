261ba46a8a43abe5e5b551a00f095aa2f91e490d
Jovan <jovan0042@gmail.com>
2019-04-10 14:03:05 +0200

    Frontend

---
+++ src/main/resources/static/precache-manifest.dcf84ca1cd444dbd93cc8f0b0f4e5c21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "dad606b0768ba57ae81c",
+    "url": "/static/js/main.83e4eb19.chunk.js"
+  },
+  {
+    "revision": "b7d6935f7e3bdf9cd6d7",
+    "url": "/static/js/2.8baa3983.chunk.js"
+  },
+  {
+    "revision": "dad606b0768ba57ae81c",
+    "url": "/static/css/main.959e4340.chunk.css"
+  },
+  {
+    "revision": "b7d6935f7e3bdf9cd6d7",
+    "url": "/static/css/2.9e3ce87e.chunk.css"
+  },
+  {
+    "revision": "e215cdda67ef7b88fd1fb6d0177401cd",
+    "url": "/index.html"
+  }
+];
